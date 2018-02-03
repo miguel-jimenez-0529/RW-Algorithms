74ba8dbe4cbae42d01804d7c6867c1b11e30c1d6
Miguel Jimenez <migueljimenez@imac-de-maria.local>
2018-02-03 10:43:52 -0600

    Initial Commit

---
+++ Console Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Console Swift
+//
+//  Created by Miguel Jimenez on 2/3/18.
+//  Copyright © 2018 Miguel Jimenez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
